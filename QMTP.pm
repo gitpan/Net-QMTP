000aba8b6666fee5fae35ae0515a733d97351e53
James Raftery <james@now.ie>
2003-01-28 19:18:38 +0000

    Import of JRAFTERY/Net-QMTP-0.02 from CPAN.
    gitpan-cpan-distribution: Net-QMTP
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         JRAFTERY/Net-QMTP-0.02.tar.gz
    gitpan-cpan-author:       JRAFTERY
    gitpan-cpan-maturity:     released

--- QMTP.pm
+++ QMTP.pm
@@ -11,22 +11,30 @@ use Carp;
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 # Please submit bug reports, patches and comments to the author.
-# Latest information at http://romana.now.ie/
+# Latest information at http://romana.now.ie/#net-qmtp
 #
-# $Id: QMTP.pm,v 1.4 2003/01/27 17:20:18 james Exp $
+# $Id: QMTP.pm,v 1.8 2003/01/28 19:11:44 james Exp $
 #
-# This module is an object interface to the Quick Mail Transfer
-# Protocol (QMTP). See the Net::QMTP man page that was installed with
-# this module for more information.
+# This module is an object interface to the Quick Mail Transfer Protocol
+# (QMTP). QMTP is a replacement for the Simple Mail Transfer Protocol
+# (SMTP). It offers increased speed, especially over high latency
+# links, pipelining, 8-bit data transmission and predeclaration of
+# line-ending encoding.
+#
+# See the Net::QMTP man page that was installed with this module for
+# information on how to use the module.
 #
 
 use vars qw($VERSION);
-$VERSION = "0.01";
+$VERSION = "0.02";
 
 sub new {
-	my $proto = shift;
+	my $proto = shift or croak;
 	my $class = ref($proto) || $proto;
-	my $host = shift || or croak "No host specified in constructor";
+	my $host = shift or croak "No host specified in constructor";
+	my %args;
+
+	%args = @_ if @_;
 	my $self = {
 		SENDER		=> undef,
 		RECIPIENTS	=> [],
@@ -35,17 +43,54 @@ sub new {
 		ENCODING	=> undef,
 		SOCKET		=> undef,
 		HOST		=> undef,
+		DEBUG		=> undef,
 	};
+	$self->{DEBUG} = 1 if $args{'Debug'};
+	$self->{HOST} = $host or croak "Constructor host() failed";
 	bless($self, $class);
-	$self->encoding("__GUESS__") or croak "Constructor encoding() failed";
-	$self->host($host) or croak "Constructor host() failed";
+	unless ($self->encoding("__GUESS__")) {
+		carp "Constructor encoding() failed";
+		return undef;
+	}
+	if ($args{'ManualConnect'}) {
+		$self->reconnect() or return undef;
+	}
 	return $self;
+}
+
+sub reconnect {
+	my $self = shift;
+	ref($self) or croak;
+
+	# can't reconnect if connected
+	return undef if $self->{SOCKET};
+	my $sock = IO::Socket::INET->new(
+			PeerAddr	=> $self->{HOST},
+			PeerPort	=> 'qmtp(209)',
+			Proto		=> 'tcp') or return undef;
+	$sock->autoflush();
+	$self->{SOCKET} = $sock;
+	return $self->{SOCKET};
+}
+
+sub disconnect {
+	my $self = shift;
+	ref($self) or croak;
+
+	# can't disconnect if not connected
+	my $sock = $self->{SOCKET} or return undef;
+	unless (close $sock) {
+		carp "Cannot close socket: $!";
+		return undef;
+	}
+	$self->{SOCKET} = undef;
+	return 1;
 }
 
 sub encoding {
 	my $self = shift;
-	ref($self) || die;
-	my $e = shift || return $self->{ENCODING};
+	ref($self) or croak;
+	my $e = shift or return $self->{ENCODING};
 
 	# guess from input record seperator
 	if ($e eq "__GUESS__") {
@@ -70,28 +115,28 @@ sub encoding {
 
 sub host {
 	my $self = shift;
-	ref($self) || die;
+	ref($self) or croak;
 	$self->{HOST} = shift if @_;
 	return $self->{HOST};
 }
 
 sub sender {
 	my $self = shift;
-	ref($self) || die;
+	ref($self) or croak;
 	$self->{SENDER} = shift if @_;
 	return $self->{SENDER};
 }
 
 sub recipient {
 	my $self = shift;
-	ref($self) || die;
+	ref($self) or croak;
 	push(@{$self->{RECIPIENTS}}, shift) if @_;
 	return $self->{RECIPIENTS};
 }
 
 sub message {
 	my $self = shift;
-	ref($self) || die;
+	ref($self) or croak;
 	if ($self->{MSGFILE}) {
 		carp "Message already created by message_from_file()";
 		return undef;
@@ -102,56 +147,80 @@ sub message {
 
 sub message_from_file {
 	my $self = shift;
-	ref($self) || die;
+	ref($self) or croak;
 	if ($self->{MESSAGE}) {
 		carp "Message already created by message()";
 		return undef;
 	}
-	my $f = shift || return $self->{MSGFILE};
-	-s $f || return undef;
+	my $f = shift or return $self->{MSGFILE};
+	-s $f or return undef;
 	$self->{MSGFILE} = $f;
 	return $self->{MSGFILE};
 }
 
+sub new_message {
+	my $self = shift;
+	ref($self) or croak;
+
+	$self->{MESSAGE} = undef;
+	$self->{MSGFILE} = undef;
+	return 1;
+}
+
+sub new_envelope {
+	my $self = shift;
+	ref($self) or croak;
+
+	$self->{SENDER} = undef;
+	$self->{RECIPIENTS} = [];
+	return 1;
+}
+
 sub _send_file {
 	my $self = shift;
-	ref($self) || die;
+	ref($self) or die;
 	my $f = $self->{MSGFILE};
 	my $sock = $self->{SOCKET};
 
-	my $size = -s $f || die;
-	open(F,$f) || die;
-	print $sock ($size+1) . ":" . $self->{ENCODING};
-	while (<F>) { print $sock };
-	print $sock ",";
-	close F || die;
+	unless (open(FILE, $f)) {
+		carp "Cannot open file '$f': $!";
+		return undef;
+	}
+	my $size = -s $f;
+	carp "File '$f' is empty" if $size == 0;
+	return undef if $size < 0;
+
+	print $sock ($size+1) . ":" . $self->{ENCODING} or return undef;
+	# count len as we read?
+	while (<FILE>) { print $sock or return undef };
+	print $sock "," or return undef;
+	unless (close FILE) {
+		carp "Cannot close file '$f': $!";
+		return undef;
+	}
+	return 1;
 }
 
 sub send {
 	my $self = shift;
-	ref($self) || die;
+	ref($self) or croak;
 
-	my $sock = IO::Socket::INET->new(
-			PeerAddr	=> $self->{HOST},
-			PeerPort	=> 'qmtp(209)',
-			Proto		=> 'tcp') or die;
-	$self->{SOCKET} = $sock;
-	$sock->autoflush();
+	$self->_ready_to_send() or return undef;
+	my $sock = $self->{SOCKET};
 
 	if ($self->{MSGFILE}) {
-		$self->_send_file();
+		$self->_send_file() or return undef;
 	} else {
-		print $sock &_as_netstring($self->{ENCODING}.$self->{MESSAGE});
+		print $sock &_as_netstring($self->{ENCODING} .
+			$self->{MESSAGE}) or return undef;
 	}
 
-	print $sock &_as_netstring($self->{SENDER});
-	print $sock &_list_as_netstring($self->{RECIPIENTS});
+	print $sock &_as_netstring($self->{SENDER}) or return undef;
+	print $sock &_list_as_netstring($self->{RECIPIENTS}) or return undef;
 
 	my($s, %r);
-
 	foreach (@{$self->{RECIPIENTS}}) {
-		read($sock, $s, $self->_getlen());
-		$self->_getcomma();
+		$s = $self->_read_netstring();
 		CASE: {
 			$s =~ s/^K/success: / and last CASE;
 			$s =~ s/^Z/deferral: / and last CASE;
@@ -160,8 +229,28 @@ sub send {
 		$r{$_} = $s;
 	}
 
-	$self->{SOCKET} = undef;
 	return \%r;
+}
+
+sub _ready_to_send {
+	my $self = shift;
+	ref($self) or die;
+
+	# need defined sender (don't need true; empty string is valid),
+	# recipient(s), message and socket
+	return (defined($self->{SENDER}) and scalar(@{$self->{RECIPIENTS}}) and
+		($self->{MESSAGE} or $self->{MSGFILE}) and
+		$self->{SOCKET});
+}
+
+sub _read_netstring {
+	my $self = shift;
+	ref($self) or die;
+	my $sock = $self->{SOCKET};
+	my $s;
+	read($sock, $s, $self->_getlen()) or die;
+	$self->_getcomma() or die;
+	return $s;
 }
 
 sub _as_netstring {
@@ -170,7 +259,7 @@ sub _as_netstring {
 }
 
 sub _list_as_netstring {
-	my $listref = shift;
+	my $listref = shift || [];
 	my $netstring;
 
 	foreach (@{$listref}) {
@@ -181,29 +270,42 @@ sub _list_as_netstring {
 
 sub _getlen {
 	my $self = shift;
-	ref($self) || die;
+	ref($self) or die;
 	my $sock = $self->{SOCKET};
 	my $len = 0;
 	my $s = "";
 	for (;;) {
 		defined(read($sock, $s, 1)) or die;
 		return $len if $s eq ":";
-		##if (len > 200000000) resources();
+		_badproto() if $s !~ /[0-9]/;
+		_badresources() if $len > 200000000;
 		$len = 10 * $len + $s;
 	}
+	return 0;
 }
 
 sub _getcomma {
 	my $self = shift;
-	ref($self) || die;
+	ref($self) or die;
 	my $sock = $self->{SOCKET};
 	my $s = "";
 	defined(read($sock, $s, 1)) or die;
-	badproto() if ($s ne ",");
+	_badproto() if $s ne ",";
+	return 1;
 }
 
 sub _badproto {
-	die "Protocol violation\n";
+	confess "Protocol violation";
+}
+
+sub _badresources {
+	confess "Excessive resources requested";
+}
+
+sub DESTROY {
+	my $self = shift;
+	ref($self) or die;
+	$self->disconnect();	# don't care about failure
 }
 
 1;
@@ -230,8 +332,12 @@ Net::QMTP - Quick Mail Transfer Protocol (QMTP) client
  $qmtp->message_from_file($filename);
 
  $qmtp->host('server.example.org');
+ $qmtp->new_envelope();
+ $qmtp->new_message();
 
+ $qmtp->reconnect()
  $qmtp->send();
+ $qmtp->disconnect()
 
 =head1 DESCRIPTION
 
@@ -243,11 +349,13 @@ email by QMTP.
 
 =over 4
 
-=item Net::QMTP->new(HOST)
+=item new(HOST)
 
-The new() constructor creates an new Net::QMTP object and returns a
-reference to it if successful, undef otherwise. C<HOST> is an FQDN or
-IP address of the QMTP server to connect to and it is mandatory.
+The C<new()> constructor creates an new Net::QMTP object and returns a
+reference to it if successful, undef otherwise. C<HOST> is an FQDN or IP
+address of the QMTP server to connect to and it is mandatory. The TCP
+session is established when the object is created but may be brought up
+and down at will by C<disconnect()> and C<reconnect()> methods.
 
 =back
 
@@ -257,14 +365,16 @@ IP address of the QMTP server to connect to and it is mandatory.
 
 =item sender(ADDRESS) sender()
 
-Return the envelope sender for this object or set it to the
-supplied C<ADDRESS>. Returns undef if the sender is not yet defined.
+Return the envelope sender for this object or set it to the supplied
+C<ADDRESS>. Returns undef if the sender is not yet defined. An empty
+envelope sender is quite valid. If you want this, be sure to call
+C<sender()> with an argument of an empty string.
 
 =item recipient(ADDRESS) recipient()
 
 If supplied, add C<ADDRESS> to the list of envelope recipients. If not,
-return a reference to the current list of recipients. Returns a reference
-to an empty list if recipients have not yet been defined.
+return a reference to the current list of recipients. Returns a
+reference to an empty list if recipients have not yet been defined.
 
 =item host(SERVER) host()
 
@@ -274,20 +384,22 @@ to. If not, return the current server.
 =item message(TEXT) message()
 
 If supplied, append C<TEXT> to the message body. If not, return the
-current message body. It is the programmer's responsibility to create
-a valid message including appropriate RFC2822/RFC822 header lines.
+current message body. It is the programmer's responsibility to create a
+valid message including appropriate RFC 2822/822 header lines.
 
 This method cannot be used on a object which has had a message body
-created by the C<message_from_file> method.
+created by the C<message_from_file()> method. Use C<new_message()> to
+erase the current message contents.
 
 =item message_from_file(FILE)
 
 Use the contents of C<FILE> as the message body. It is the programmer's
 responsibility to create a valid message in C<FILE> including
-appropriate RFC2822/RFC822 header lines.
+appropriate RFC 2822/822 header lines.
 
 This method cannot be used on a object which has had a message body
-created by the C<message> method.
+created by C<message()>. Use C<new_message()> to erase the current
+message contents.
 
 =item encoding(TYPE) encoding()
 
@@ -296,22 +408,22 @@ Set the line-ending encoding for this object to one of:
 B<unix> - Unix-like line ending; lines are delimited by a line-feed
 character.
 
-B<dos> - DOS/Windows line ending; lines are delimited by a carraige-return
-line-feed character pair.
+B<dos> - DOS/Windows line ending; lines are delimited by a
+carraige-return line-feed character pair.
 
-The C<new> method will make a guess at which encoding to use based on the
-value of C<$/>. Call C<encoding> method without an argument to get the
+The constructor will make a guess at which encoding to use based on
+the value of C<$/>. Call C<encoding()> without an argument to get the
 current line-encoding. It will return a line-feed for C<unix>, a
 carraige-return for C<dos> or undef if the encoding couldn't be set.
 
-Be sure the messages you create with C<message> and C<message_from_file>
-have approproiate line-endings.
+Be sure the messages you create with C<message()> and
+C<message_from_file()> have approproiate line-endings.
 
 =item send()
 
-Send the message. It returns a reference to a hash. The hash is
-keyed by recipient address. The value for each key is the response
-from the QMTP server, prepended with one of:
+Send the message. It returns a reference to a hash. The hash is keyed by
+recipient address. The value for each key is the response from the QMTP
+server, prepended with one of:
 
 B<success:> - the message was accepted for delivery
 
@@ -320,7 +432,52 @@ B<deferral:> - temporary failure. The client should try again later
 B<failure:> - permanent failure. The message was not accepted and should
 not be tried again
 
+See L<"EXAMPLES">.
+
+=item new_envelope()
+
+Reset the object's envelope information; sender and recipients. Does
+not affect the message body.
+
+=item new_message()
+
+Reset the object's message information; message text or message file.
+Does not affect the envelope.
+
+=item disconnect()
+
+Close the network connection to the object's server. Returns undef if
+this fails. The object's destructor will call C<disconnect()> to be sure
+any open socket is closed cleanly when the object is destroyed.
+
+=item reconnect()
+
+Reestablish a network connection to the object's server. Returns undef
+if the connection could not be established.
+
 =back
+
+=head1 EXAMPLES
+
+ use Net::QMTP;
+ my $qmtp = Net::QMTP->new('server.example.org') or die;
+
+ $qmtp->sender('sender@example.org');
+ $qmtp->recipient('joe@example.org');
+ $qmtp->message('From: sender@example.org' . "\n" .
+ 		'To: joe@example.org' . "\n" .
+		"Subject: QMTP test\n\n" .
+		"Hi Joe!\nThis message was sent over QMTP");
+
+ my $response = $qmtp->send() or die;
+ foreach (keys %{ $response }) {
+	 print $_ . ": " . ${$response}{$_} . "\n";
+ }
+ $qmtp->disconnect();
+
+=head1 SEE ALSO
+
+L<qmail-qmtpd(8)>, L<maildirqmtp(1)>.
 
 =head1 NOTES
 
@@ -329,7 +486,7 @@ The QMTP protocol is described in http://cr.yp.to/proto/qmtp.txt
 QMTP is a replacement for SMTP and, as such, requires a QMTP server in
 addition to this client. The qmail MTA includes a QMTP server;
 qmail-qmtpd. Setting up the server is outside the scope of the module's
-documentation.
+documentation. See http://www.qmail.org/ for more QMTP information.
 
 =head1 CAVEATS
 
@@ -339,25 +496,48 @@ platforms.
 If, on a Unix system, you say:
 
  $qmtp->encoding("dos");
- 
-with the intention of later supplying a DOS formatted file, don't
-make the mistake of substituting C<message_from_file> with something
-like:
+
+with the intention of later supplying a DOS formatted file, don't make
+the mistake of substituting C<message_from_file> with something like:
 
  $qmtp->message($lineone . "\n" . $linetwo);
 
-On Unix systems C<\n> is (only) a line-feed. You should either explicitly
-change the encoding back to C<unix> or supply your text with the proper
-encoding:
+On Unix systems C<\n> is (only) a line-feed. You should either
+explicitly change the encoding back to C<unix> or supply your text with
+the proper encoding:
 
  $qmtp->message($lineone . "\r\n" . $linetwo);
+
+=head1 BUGS
+
+Also known as the TODO list:
+
+=over 4
+
+=item *
+
+we have no timeouts
+
+=item *
+
+we have no debugging output
+
+=item *
+
+we need an option to constructor to set socket timeout
+
+=item *
+
+how do we reset client/server state if failure occurs during transmission?
+
+=item *
+
+we should write more tests
+
+=back
 
 =head1 AUTHOR
 
 James Raftery <james@now.ie>.
-
-=head1 SEE ALSO
-
-L<qmail-qmtpd(8)>, L<maildirqmtp(1)>.
 
 =cut
